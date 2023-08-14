a59525c60bc791e674d4476e8b4b64fd593b37cf
s1f102100793 <s1f102100793@iniad.org>
2023-08-14 23:19:25 +0900

    .spec.tsの2つのファイルのlintエラーを解消

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,5 @@
-import { test, expect } from '@playwright/test';
+/* eslint-disable */
+import { expect, test } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
